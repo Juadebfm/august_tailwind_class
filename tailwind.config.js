8e5679170a22b0ab8434ff3625b84b6f2119899a
Rukayat Adeyemi Abiodun <rukayatadeyemiabiodun@gmail.com>
2024-11-02 18:11:29 -0700

    feat:completed nav and hero section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dm_sans: ["DM Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        p_bg: "#2F1893",
+        p_nav_btn: "#482BE7",
+        p_hero_btn: "#25DAC5",
+        nav_blue: "#1E0E62"
+      },
+      fontSize: "24px",
+    },
+  },
+  plugins: [],
+};
