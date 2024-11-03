1b73af71dfdf535465ea24b424b9907e6714d2a1
Julius Adebowale <juadebgabriel@gmail.com>
2024-11-03 17:19:06 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        job_cat_bg: "#161C2D",
+        job_cat_box_blue: "#473BF0",
+        job_cat_box_offwhite: "#E7E9ED",
+        job_cat_explore: "#68D585",
+      },
+      fontFamily: {
+        gilroy_regular: ["Gilroy_Regular", "sans-serif"],
+        gilroy_medium: ["Gilroy_Medium", "sans-serif"],
+        gilroy_semibold: ["Gilroy_Semibold", "sans-serif"],
+        gilroy_bold: ["Gilroy_Bold", "sans-serif"],
+        gilroy_black: ["Gilroy_Black", "sans-serif"],
+        gilroy_light: ["Gilroy_Light", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
